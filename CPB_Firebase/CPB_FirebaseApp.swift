0b7c9e5786bcafdcd4dd8dbe6e2b3f3be47933e4
valeriemorales <vmorales4297@stu.d214.org>
2023-11-06 10:51:25 -0600

    Initial Commit

---
+++ CPB_Firebase/CPB_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPB_FirebaseApp.swift
+//  CPB_Firebase
+//
+//  Created by Valerie Morales on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CPB_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPB_Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPB_Firebase
+//
+//  Created by Valerie Morales on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
